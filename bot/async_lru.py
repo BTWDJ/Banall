800c0773684e08b1f5d14cadd44f8446d8bc3d46
BTWDJ <119585273+BTWDJ@users.noreply.github.com>
2024-07-20 02:17:06 +0530

    Create async_lru.py

---
+++ bot/async_lru.py
@@ -0,0 +1,22 @@
+import asyncio
+from functools import wraps
+
+def alru_cache(maxsize=128, typed=False, **kwargs):
+    def decorator(func):
+        cache = {}
+
+        @wraps(func)
+        async def wrapped(*args, **kwargs):
+            key = args + tuple(kwargs.items())
+            if key in cache:
+                return cache[key]
+            result = await func(*args, **kwargs)
+            if len(cache) >= maxsize:
+                cache.pop(next(iter(cache)))
+            cache[key] = result
+            return result
+
+        return wrapped
+
+    return decorator
+  
